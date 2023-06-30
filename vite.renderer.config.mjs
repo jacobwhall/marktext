1e728f07e24f3fc6973743660f81af4fa3b17349
Axel Rindle <mail@axelrindle.de>
2023-06-30 19:36:11 +0200

    Externalize builtin modules

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
+import { builtinModules } from 'module'
 
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [vue()],
   build: {
     rollupOptions: {
-      external: [
-        'ced',
-        'fontmanager-redux'
-      ]
+      external: builtinModules
     }
   }
 })
