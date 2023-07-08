0d9ec833c8afa5ac7841f90becbaa7da0b2cf196
Axel Rindle <mail@axelrindle.de>
2023-07-08 22:46:08 +0200

    Remove log

--- src/main/utils/static.js
+++ src/main/utils/static.js
@@ -2,6 +2,5 @@ import { app } from 'electron'
 import path from 'path'
 
 export default function __static () {
-  console.log(app.getAppPath())
   return path.join(app.getAppPath(), '/static').replace(/\\/g, '\\\\')
 }
